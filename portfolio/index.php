32a8adff4174ff9392b80847201e3d252f6ef317
pefem <50462172+pefem@users.noreply.github.com>
2020-04-28 15:44:31 +0100

    Add files via upload

---
+++ portfolio/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="https://kit.fontawesome.com/58ccc28875.js" crossorigin="anonymous"></script>
+    <title>My Portfolio</title>
+</head>
+<body id="bg-img">
+    <div class = "overlay"></div>
+    <header>
+        <div class ="menu-btn">
+            <div class = "btn-line"></div>
+            <div class = "btn-line"></div>
+            <div class = "btn-line"></div>
+        </div>
+
+        <nav class = "menu">
+            <div class ="menu-branding">
+                <div class = "portrait">
+                </div>
+            </div>
+            <ul class = "menu-nav">
+                <li class = "nav-item current">
+                    <a href = "#" class="nav-link">HOME</a>
+                </li>
+
+                <li class = "nav-item">
+                    <a href = "#" class="nav-link">ABOUT ME</a>
+                </li>
+
+                <li class = "nav-item">
+                    <a href = "#" class="nav-link">MY WORK</a>
+                </li>
+
+                <li class = "nav-item">
+                    <a href = "#" class="nav-link">CONTACT ME</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main id = "home">
+        <h1 class = "lg-heading">Philip <span class = "text-secondary">Efem</span></h1>
+        <h2 class = "sm-heading">Software Developer, Business Analyst</h2>
+        <div class = "icons">
+            <a href ="#">
+                <i class = "fab fa-twitter fa-2x"></i>
+            </a>
+
+            <a href ="#">
+                <i class = "fab fa-facebook fa-2x"></i>
+            </a>
+
+            <a href ="#">
+                <i class = "fab fa-linkedin fa-2x"></i>
+            </a>
+
+            <a href ="#">
+                <i class = "fab fa-github fa-2x"></i>
+            </a>
+
+        </div>
+    </main>
+
+    <script src = "js/main.js"></script>
+</body>
+</html>
